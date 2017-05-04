6f47eb79cac94d8ecc7a62d0a9ec14c59df83b04
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-05-04 17:51:03 +0900

    Tiny changes. Add pjv to devDependencies. Improve demo page.

--- dist/jquery.simple-scroll-follow.js
+++ dist/jquery.simple-scroll-follow.js
@@ -1,6 +1,6 @@
 /**
  * @file jQuery Plugin: jquery.simple-scroll-follow
- * @version 3.0.0
+ * @version 3.0.1
  * @author Yuusaku Miyazaki <toumin.m7@gmail.com>
  * @license MIT License
  */
--- sample/sample.js
+++ sample/sample.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($) {
+$(function () {
   // ページ内リンクのスクロール
-  $('a[href^="#"]').click(function() {
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href === "" ? 'html' : href);
-    var position = target.offset().top - 10;
-    $('body,html').animate({scrollTop: position}, 200, 'swing');
+  $(document).on('click', 'a[href^="#"]', function() {
+    var href= $(this).attr('href');
+    var target = $((href == '#' || href === '') ? 'html' : href);
+    var position = target.offset().top;
+    $('body, html').animate({scrollTop: position}, 200, 'swing');
     history.pushState('', '', $(this)[0].href);
     return false;
   });
@@ -22,5 +22,12 @@ jQuery(document).ready(function($) {
   $('nav').simpleScrollFollow({
     min_width: 992,
     limit_elem: $('article')
+  });
+
+  // 見出し横のリンクを生成
+  $('section.panel-primary').each(function() {
+    var link = $('<a class="js-anchor"><span class="glyphicon glyphicon-link" aria-hidden="true"></span></a>');
+    $(link).attr('href', '#' + $(this).attr('id'));
+    $(this).find('h3.panel-title').prepend(link);
   });
 });
